47a828155770f89db6ffd075d75d392570c37a43
Sushant <ksoe-codes@email.com>
2023-05-08 10:08:58 +0530

    Day 1

---
+++ 100daysofcode/demo.py
@@ -0,0 +1,4 @@
+print("Welcome to Band name generator.")
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("\nWhat's your pet's name?\n")
+print('Your band name could be ' + city_name + ' ' + pet_name)
