a39681c91b41f391c5294782ae1b507a7fdcc6b9
Sushant <ksoe-codes@email.com>
2023-05-09 01:09:36 +0530

    Day 3

---
+++ leapyear.py
@@ -0,0 +1,12 @@
+year = int(input("Enter the year: "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        print(f"{year} is not a leap year.")
+    elif year % 400 == 0:
+        print(f"{year} is a leap year.")
+    else:
+        print(f"{year} is a leap year.")
+        
+else:
+    print(f"{year} is not a leap year.")
