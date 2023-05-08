a2a0776c03ad694534bd8a0930cdd97f1c5e48da
Sushant <ksoe-codes@email.com>
2023-05-08 18:12:23 +0530

    Day 2

--- TipCalculator.py
+++ TipCalculator.py
@@ -1,11 +1,11 @@
 print("Welcome to Tip Calculator.")
-bill = int(input("What was the total bill: "))
-tip = input("What percentage of tip would you like to give? ")
-tip_int = int(tip)/100
-tip_amt = bill * tip_int
+bill = float(input("What was the total bill: "))
+tip = int(input("What percentage of tip would you like to give? "))
+tip_perc = tip/100
+tip_amt = bill * tip_perc
 
 people = int(input("How many people to split the bill? "))
 
 total_amount_perperson = (bill + tip_amt)/people
-
-print("Each person should pay: " + str(total_amount_perperson))
+final_bill_amount = round(total_amount_perperson,2)
+print("Each person should pay: " + str(final_bill_amount))
